1abe86f986ede2d08cd495666bb98d8e5dd1d3fe
Mauri de Souza Meneguzzo <mauri870@gmail.com>
2021-03-16 11:22:49 -0300

    feat: new integration using onMessage and onError/onReaDy callbacks

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import PlaidLink from './PlaidLink'
-import Config from './config'
 
 export default function App() {
   return (
     <PlaidLink
-      linkToken={Config.TEST_LINK_TOKEN}
+      linkToken='link-sandbox-f6e9d5a0-f496-49b8-8341-b5ef25a99fec'
       onEvent={(event) => console.log(event)}
       onExit={(exit) => console.log(exit)}
       onSuccess={(success) => console.log(success)}
--- PlaidLink.tsx
+++ PlaidLink.tsx
@@ -1,8 +1,7 @@
 import React, { useRef } from 'react'
-import { WebView } from 'react-native-webview'
+import { WebView, WebViewErrorEvent } from 'react-native-webview'
 import {
   LinkEvent,
-  LinkEventName,
   LinkExit,
   LinkSuccess,
   LinkErrorCode,
@@ -14,58 +13,83 @@ interface PlaidLinkProps {
   linkToken: string
   onEvent?(event: LinkEvent): any
   onExit?(exit: LinkExit): any
+  onReady?(): any
+  onError?(event: WebViewErrorEvent, webview: any): any
   onSuccess?(success: LinkSuccess): any
 }
+
+const injectedJavaScript = `(function() {
+  window.postMessage = function(data) {
+    window.ReactNativeWebView.postMessage(data);
+  };
+})()`;
 
 export default function PlaidLink({
   linkToken,
   onEvent,
   onExit,
+  onReady,
+  onError,
   onSuccess,
 }: PlaidLinkProps) {
   let webviewRef: any = useRef()
 
   const handleNavigationStateChange = (event: any) => {
-    if (event.url.startsWith('plaidlink://')) {
-      console.log(event.url)
-      const eventParams = queryString.parse(event.url)
+    if (event.url.startsWith('https://cdn.plaid.com')) {
+      return true;
+    }
 
-      const linkSessionId = eventParams.link_session_id as string
-      const mfaType = eventParams.mfa_type as string
-      const requestId = eventParams.request_id as string
-      const viewName = eventParams.view_name as string
-      const errorCode = eventParams.error_code as string
-      const errorMessage = eventParams.error_message as string
-      const errorType = eventParams.error_type as string
-      const exitStatus = eventParams.exist_status as string
-      const institutionId = eventParams.institution_id as string
-      const institutionName = eventParams.institution_name as string
-      const institutionSearchQuery = eventParams.institution_search_query as string
-      const timestamp = eventParams.timestamp as string
+    return false
+  }
 
-      if (event.url.startsWith('plaidlink://event') && onEvent) {
-        onEvent({
-          eventName: eventParams.event_name as LinkEventName,
-          metadata: {
-            linkSessionId,
-            mfaType,
-            requestId,
-            viewName,
-            errorCode,
-            errorMessage,
-            errorType,
-            exitStatus,
-            institutionId,
-            institutionName,
-            institutionSearchQuery,
-            timestamp,
-          },
-        })
-      } else if (event.url.startsWith('plaidlink://exit') && onExit) {
-        onExit({
+  const handlePlaidEvent = (payload: any) => {
+    if (!payload.action) {
+      return;
+    }
+
+    const eventName = payload.action.split('::')[1] as string;
+
+    if (eventName == 'acknowledged') {
+      return
+    }
+
+    const linkSessionId = payload.link_session_id as string
+    const mfaType = payload.mfa_type as string
+    const requestId = payload.request_id as string
+    const viewName = payload.view_name as string
+    const errorCode = payload.error_code as string
+    const errorMessage = payload.error_message as string
+    const errorType = payload.error_type as string
+    const exitStatus = payload.exist_status as string
+    const institutionId = payload.institution_id as string
+    const institutionName = payload.institution_name as string
+    const institutionSearchQuery = payload.institution_search_query as string
+    const timestamp = payload.timestamp as string
+
+    onEvent && onEvent({
+      eventName,
+      metadata: {
+        linkSessionId,
+        mfaType,
+        requestId,
+        viewName,
+        errorCode,
+        errorMessage,
+        errorType,
+        exitStatus,
+        institutionId,
+        institutionName,
+        institutionSearchQuery,
+        timestamp,
+      },
+    })
+
+    switch (eventName) {
+      case 'exit':
+        onExit && onExit({
           error: {
             errorCode: LinkErrorCode[errorCode as keyof typeof LinkErrorCode],
-            errorMessage: eventParams.error_message as string,
+            errorMessage: payload.error_message as string,
             errorType: LinkErrorType[errorType as keyof typeof LinkErrorType],
           },
           metadata: {
@@ -81,10 +105,11 @@ export default function PlaidLink({
             requestId,
           },
         })
-      } else if (event.url.startsWith('plaidlink://connected') && onSuccess) {
-        const publicToken = eventParams.public_token as string
-        const accounts = JSON.parse(eventParams.accounts as string)
-        onSuccess({
+        break
+      case 'connected':
+        const publicToken = payload.metadata.public_token as string
+        const { accounts } = payload.metadata;
+        onSuccess && onSuccess({
           publicToken,
           metadata: {
             institution: {
@@ -95,21 +120,30 @@ export default function PlaidLink({
             linkSessionId,
           },
         })
-      }
-      return false
+        break;
+      case 'ready':
+        onReady && onReady()
+      case 'acknowledged':
+        break
+      case 'event':
+        break
+      default:
+        console.warn('Unhandled plaid event: ', payload)
     }
-    return true
   }
 
   return (
     <WebView
       source={{
-        uri: `https://cdn.plaid.com/link/v2/stable/link.html?isWebview=true&token=${linkToken}`,
+        uri: `https://cdn.plaid.com/link/v2/stable/link.html?isWebView=true&token=${linkToken}`,
       }}
       ref={(ref) => (webviewRef = ref)}
-      onError={ () => webviewRef.reload() }
-      originWhitelist={['https://*', 'plaidlink://*']}
+      onError={(event: WebViewErrorEvent) => onError && onError(event, webviewRef)}
+      onReady={() => onReady && onReady() }
+      originWhitelist={['https://*']}
       onShouldStartLoadWithRequest={handleNavigationStateChange}
+      injectedJavaScript={injectedJavaScript}
+      onMessage={(e)=> handlePlaidEvent(JSON.parse(e.nativeEvent.data))}
     />
   )
 }
